e1b30a3f4712656e2ca6f7a88a908a51c4570b2d
吴云鹏 <41565182@qq.com>
2020-11-22 23:10:03 +0800

    新建的Servlet

---
+++ web1/src/wyp/ResponseDemo1.java
@@ -0,0 +1,23 @@
+package wyp;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author:吴云鹏@Date:2020/11/22 - Time:23:01
+ * @ DESCRIPTION
+ */
+@WebServlet(name = "ResponseDemo1")
+public class ResponseDemo1 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
